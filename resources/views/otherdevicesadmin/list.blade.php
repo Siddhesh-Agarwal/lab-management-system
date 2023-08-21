086c5fbd92391b3c96b0e47f973609ebc9e72cf2
SHREE VARSHAN G <727721eucs140@skcet.ac.in>
2023-08-21 16:13:34 +0530

    changes heavy

--- resources/views/otherdevicesadmin/list.blade.php
+++ resources/views/otherdevicesadmin/list.blade.php
@@ -9,9 +9,7 @@
             padding: 10px;
         }
 
-        table th {
-            color: black;
-        }
+       
 
         body {
             margin-top: 75px;
@@ -26,29 +24,29 @@
     <section class="content">
         <div class="container-fluid" style="margin-top: 100px">
             @if (Session::has('success'))
-                    <div id="success-alert" class="alert alert-success" role=alert>
-                        {{ Session::get('success') }}
-                    </div>
-                    <script>
-                        // Auto-close the success alert after 5 seconds
-                        setTimeout(function() {
-                            $('#success-alert').fadeOut('slow');
-                        }, 5000);
+                <div id="success-alert" class="alert alert-success" role=alert>
+                    {{ Session::get('success') }}
+                </div>
+                <script>
+                    // Auto-close the success alert after 5 seconds
+                    setTimeout(function() {
+                        $('#success-alert').fadeOut('slow');
+                    }, 5000);
 
-                        // Auto-close the error alert after 5 seconds
-                    </script>
-                @endif
+                    // Auto-close the error alert after 5 seconds
+                </script>
+            @endif
 
-                @if (Session::has('error'))
-                    <div id="error-alert" class="alert alert-danger" role=alert>
-                        {{ Session::get('error') }}
-                    </div>
-                    <script>
-                        setTimeout(function() {
-                            $('#error-alert').fadeOut('slow');
-                        }, 5000);
-                    </script>
-                @endif
+            @if (Session::has('error'))
+                <div id="error-alert" class="alert alert-danger" role=alert>
+                    {{ Session::get('error') }}
+                </div>
+                <script>
+                    setTimeout(function() {
+                        $('#error-alert').fadeOut('slow');
+                    }, 5000);
+                </script>
+            @endif
             <div class="row">
                 @foreach ($data as $key => $dev)
                     <div class="col-12 col-sm-6 col-md-6">
