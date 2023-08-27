d670769af6434ade0ffd0c714e38d8a5748f0b24
SHREE VARSHAN G <727721eucs140@skcet.ac.in>
2023-08-28 00:06:20 +0530

    labs

--- resources/views/superadmin/addlablist.blade.php
+++ resources/views/superadmin/addlablist.blade.php
@@ -5,11 +5,6 @@
         input {
             margin-bottom: 15px;
         }
-
-        .alert {
-            background-color: rgb(177, 237, 150);
-            width: 30%;
-        }
     </style>
 
     @if (Session::has('success'))
@@ -18,20 +13,51 @@
         </div>
     @endif
 
-    <form method="post" action="{{ route('superadmin.savelabs') }}" class="row g-3" style="margin:130px; ">
-        @csrf
-        <label for="lab_name">Lab Name:</label>
-        <input type="text" class="form-control" id="lab_name" name="lab_name" placeholder="Enter Lab Name" required>
-        <label for="lab_code">Lab Code:</label>
-        <input type="text" class="form-control" id="code" name="code" placeholder="Enter Lab Code" required>
-        <label for="block">Block:</label>
-        <input type="text" class="form-control" id="block" name="block" placeholder="Enter Block" required>
-        <label for="room_number">Room Number:</label>
-        <input type="text" class="form-control" id="room_number" name="room_number" placeholder="Enter Room Number" required>
-        <label for="department">Room Number:</label>
-        <input type="text" class="form-control" id="department" name="department" placeholder="Enter Department" required>
-        <button type="submit" class="btn btn-primary">Add</button>
-        <hr>
-        <a href='{{ route('superadmin.labdetails') }}' class="btn btn-danger">Back</a>
-    </form>
+    <section class="content">
+        <div class="container">
+            <div class="container-fluid">
+                <div class="row" style="margin-top:100px;">
+                    <div class="col-12">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Add Lab</h3>
+                            </div>
+                            @if (Session::has('error'))
+                                <div id="error-alert" class="alert alert-danger" role=alert>
+                                    {{ Session::get('error') }}
+                                </div>
+                                <script>
+                                    setTimeout(function() {
+                                        $('#error-alert').fadeOut('slow');
+                                    }, 5000);
+                                </script>
+                            @endif
+                            <form method="post" action="{{ route('superadmin.savelabs') }}" class="row g-3"
+                                style="margin:30px; ">
+                                @csrf
+                                <label for="lab_name">Lab Name:</label>
+                                <input type="text" class="form-control" id="lab_name" name="lab_name"
+                                    placeholder="Enter Lab Name" required>
+                                <label for="lab_code">Lab Code:</label>
+                                <input type="text" class="form-control" id="code" name="code"
+                                    placeholder="Enter Lab Code" required>
+                                <label for="block">Block:</label>
+                                <input type="text" class="form-control" id="block" name="block"
+                                    placeholder="Enter Block" required>
+                                <label for="room_number">Room Number:</label>
+                                <input type="text" class="form-control" id="room_number" name="room_number"
+                                    placeholder="Enter Room Number" required>
+                                <label for="department">Room Number:</label>
+                                <input type="text" class="form-control" id="department" name="department"
+                                    placeholder="Enter Department" required>
+                                <button type="submit" class="btn btn-primary">Add</button>
+                                <hr>
+                                <a href='{{ route('superadmin.labdetails') }}' class="btn btn-danger">Back</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/superadmin/edit_admin.blade.php
+++ resources/views/superadmin/edit_admin.blade.php
@@ -6,87 +6,84 @@
             <div class="container-fluid">
                 <div class="row" style="margin-top:100px;">
                     <div class="col-12">
-                        <div class="card">
-                            <div class="card card-primary">
-                                <div class="card-header">
-                                    <h3 class="card-title">Edit Admin</h3>
-                                </div>
-                                @if (Session::has('error'))
-                                    <div id="error-alert" class="alert alert-danger" role=alert>
-                                        {{ Session::get('error') }}
-                                    </div>
-                                    <script>
-                                        setTimeout(function() {
-                                            $('#error-alert').fadeOut('slow');
-                                        }, 5000);
-                                    </script>
-                                @endif
-                                <!-- /.card-header -->
-                                <!-- form start -->
-                                <form action={{ route('superadmin.update.admin', $user->id) }} method="POST"
-                                    enctype="multipart/form-data">
-                                    @csrf
-                                    @method('PATCH')
-                                    <div class="card-body">
-                                        <div class="form-group">
-                                            <label for="exampleInputName">Name</label>
-                                            <input type="text" name="name" class="form-control"
-                                                value={{ $user->name }}>
-                                            @error('name')
-                                                <div class="alert alert-danger" role="alert" style="margin-top: 5px">
-                                                    <i class="fas fa-exclamation-triangle"></i>
-                                                    <span style="padding-left: 5px">{{ $message }}</span>
-                                                </div>
-                                            @enderror
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputEmail">Email</label>
-                                            <input type="email" name="email" class="form-control" id="exampleInputEmail"
-                                                value={{ $user->email }}>
-                                            @error('email')
-                                                <div class="alert alert-danger" role="alert" style="margin-top: 5px">
-                                                    <i class="fas fa-exclamation-triangle"></i>
-                                                    <span style="padding-left: 5px">{{ $message }}</span>
-                                                </div>
-                                            @enderror
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputPassword">Password</label>
-                                            <input type="password" name="password" id="password" class="form-control"
-                                                value="{{ $password }}">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputRole">Role</label>
-                                            <select name="role" id="role" class="form-control">
-                                                <option value="admin" selected>Admin</option>
-                                                <option value="superadmin">Super Admin</option>
-                                            </select>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="labname">Lab Name:</label>
-                                            <select name="labname" id="labname" class="form-control">
-                                                <option value="{{ $user->labname }}" selected>{{ $user->labname }}</option>
-                                                @foreach ($labs as $dev)
-                                                    <option value="{{ $dev->lab_name }}">{{ $dev->lab_name }}</option>
-                                                @endforeach
-                                            </select>
-                                            @error('labname')
-                                                <div class="alert alert-danger" role="alert" style="margin-top: 5px">
-                                                    <i class="fas fa-exclamation-triangle"></i>
-                                                    <span style="padding-left: 5px">{{ $message }}</span>
-                                                </div>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                    <!-- /.card-body -->
-                                    <div class="card-footer">
-                                        <button type="submit" class="btn btn-primary">Save</button>
-                                    </div>
-                                </form>
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Edit Admin</h3>
                             </div>
+                            @if (Session::has('error'))
+                                <div id="error-alert" class="alert alert-danger" role=alert>
+                                    {{ Session::get('error') }}
+                                </div>
+                                <script>
+                                    setTimeout(function() {
+                                        $('#error-alert').fadeOut('slow');
+                                    }, 5000);
+                                </script>
+                            @endif
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <form action={{ route('superadmin.update.admin', $user->id) }} method="POST"
+                                enctype="multipart/form-data">
+                                @csrf
+                                @method('PATCH')
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="exampleInputName">Name</label>
+                                        <input type="text" name="name" class="form-control" value={{ $user->name }}>
+                                        @error('name')
+                                            <div class="alert alert-danger" role="alert" style="margin-top: 5px">
+                                                <i class="fas fa-exclamation-triangle"></i>
+                                                <span style="padding-left: 5px">{{ $message }}</span>
+                                            </div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail">Email</label>
+                                        <input type="email" name="email" class="form-control" id="exampleInputEmail"
+                                            value={{ $user->email }}>
+                                        @error('email')
+                                            <div class="alert alert-danger" role="alert" style="margin-top: 5px">
+                                                <i class="fas fa-exclamation-triangle"></i>
+                                                <span style="padding-left: 5px">{{ $message }}</span>
+                                            </div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword">Password</label>
+                                        <input type="password" name="password" id="password" class="form-control"
+                                            value="{{ $password }}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputRole">Role</label>
+                                        <select name="role" id="role" class="form-control">
+                                            <option value="admin" selected>Admin</option>
+                                            <option value="superadmin">Super Admin</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="labname">Lab Name:</label>
+                                        <select name="labname" id="labname" class="form-control">
+                                            <option value="{{ $user->labname }}" selected>{{ $user->labname }}</option>
+                                            @foreach ($labs as $dev)
+                                                <option value="{{ $dev->lab_name }}">{{ $dev->lab_name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('labname')
+                                            <div class="alert alert-danger" role="alert" style="margin-top: 5px">
+                                                <i class="fas fa-exclamation-triangle"></i>
+                                                <span style="padding-left: 5px">{{ $message }}</span>
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </div>
+                            </form>
                         </div>
-                        <!-- /.card -->
                     </div>
+                    <!-- /.card -->
                 </div>
             </div>
         </div>
