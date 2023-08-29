b180b8fd2ca030c9222e98cdc88e4376503253c2
Sanjeevi-CS <sanjeevisakthivel2004@gmail.com>
2023-08-29 23:56:31 +0530

    Update footer

--- resources/views/superadmin/footer.blade.php
+++ resources/views/superadmin/footer.blade.php
@@ -1,5 +1,5 @@
         <!-- Main Footer -->
         <footer class="main-footer">
-            <strong>Copyright &copy; 2023-2025 <a href="http://skcet.ac.in">SKCET</a>.</strong>
+            <strong>Copyright &copy; 2023-2025 <a href="http://skcet.ac.in" target="_blank">SKCET</a>.</strong>
             All rights reserved.
         </footer>
