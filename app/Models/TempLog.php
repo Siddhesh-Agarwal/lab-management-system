79d65f5a2cb313ae72d0111628ae890cfbfb9802
SHREE VARSHAN G <727721eucs140@skcet.ac.in>
2023-08-31 17:03:37 +0530

    ADDITION OF TEMP - LOGS

---
+++ app/Models/TempLog.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TempLog extends Model
+{
+    use HasFactory;
+
+    protected $table = "maintenance_log";
+    protected $fillable = [
+        'device_name',
+        'serial_number',
+        'system_model_number',
+        'count',
+        'desc',
+        'lab_name',
+        'lab_id',
+        'moved_time',
+        'returned_time'
+    ];
+    
+}
---
+++ database/migrations/2023_08_31_170134_temp_logs.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('temp_log', function (Blueprint $table) {
+            $table->id();
+            $table->string('device_name');
+            $table->string('serial_number')->nullable();
+            $table->string('system_model_number');
+            $table->integer('count')->default(1);
+            $table->string('desc')->nullable();
+            $table->string('lab_name');
+            $table->unsignedBigInteger('lab_id');
+            $table->foreign('lab_id')->references('id')->on('lab__tables');
+            $table->string('moved_time');
+            $table->string('returned_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('temp_log');
+    }
+};
